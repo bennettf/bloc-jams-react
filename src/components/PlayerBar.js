e9b2efcb331dc0d0e7aeb25b3ec785c9ce98ec6e
Bennett Fong <bennettfong@mac.com>
2018-06-17 21:47:23 -0700

    volume added

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -31,17 +31,16 @@ class PlayerBar extends Component {
           </section>
 
           <section id = "volume-control">
-            <div className = "icon ion-volume-low"></div>
+            <div className = "icon ion-volume-high"></div>
             <input
               type = "range"
               className = "seek-bar"
-              value = {this.props.currentVolume}
+              value = {(this.props.volume)}
               max = "1"
               min = "0"
               step = "0.01"
               onChange={this.props.handleVolumeChange}
               />
-              <div className = "icon ion-volume-high"></div>
               </section>
         </section>
     );
